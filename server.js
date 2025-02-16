fa8eca5290df09c3c0685deebae25832093783f7
Nurdvu1et <153787451+Nurdvu1et@users.noreply.github.com>
2025-02-16 18:57:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,4 +21,6 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected locally"))
     .catch(err => console.error(err));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
