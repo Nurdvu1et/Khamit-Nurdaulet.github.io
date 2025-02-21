8991cfa8908dc719155b38d150a4e524e98355b7
Nurdvu1et <nurdaulet.xamit.bk@gmail.com>
2025-02-21 21:19:49 +0500

    Fixed server port binding issue

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, unique: true },
     password: String,
     role: { type: String, enum: ["user", "admin"], default: "user" }
 });
